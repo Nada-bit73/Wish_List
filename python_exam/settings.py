f45bef1b7627beb70e59152211bfa11068c930ac
Nada Alsogour <99046134+Nada-bit73@users.noreply.github.com>
2022-07-06 21:00:51 +0300

    Update settings.py

--- python_exam/settings.py
+++ python_exam/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q$#h0))jp(-7vmktyys20hh%wu-*zyhm$hhi4%k2zvvr0&hi^%'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
